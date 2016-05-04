51b928040730a9c668245e6a1282f4d23d409e21
C. Bess <cbess@company.com>
2016-05-04 15:27:55 -0500

    add site theme

--- client/main.js
+++ client/main.js
@@ -1,2 +1,7 @@
 import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
+import '/imports/ui/stylesheets/style.css';
+import '/imports/ui/stylesheets/responsive.css';
+import '/imports/ui/stylesheets/font-awesome.min.css';
+import '/imports/ui/stylesheets/responsive.css';
+import '/imports/ui/stylesheets/animate.min.css';
